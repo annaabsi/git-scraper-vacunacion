8209414decbf915999387973e6f0154800d840d4
Anna Absi <anna.absi@gmail.com>
2021-05-04 16:15:55 -0500

    CSVs reorganizados por DOSIS 1 y 2, ACUMULADOS 1 y 2

--- scraper.py
+++ scraper.py
@@ -9,16 +9,16 @@ df=pd.read_csv(URL_DATA)
 df.drop(['UUID', 'GRUPO_RIESGO', 'EDAD','SEXO','FABRICANTE','DIRESA','PROVINCIA','DISTRITO'], axis=1, inplace=True)
 
 # Please write the full query on one line
-# ACUMULADO DOSIS 1
-query_dosis1="""select FECHA_CORTE, FECHA_VACUNACION, DOSIS, count (rowid) as TOTAL, sum(count (rowid)) over (order by FECHA_VACUNACION) as ACUMULADO from df where "DOSIS" = 1 group by FECHA_VACUNACION """
-result_query_dosis1=ps.sqldf(query_dosis1,locals())
-print(ps.sqldf(query_dosis1,locals()))
+# DOSIS 1 y 2
+query_dosis="""select FECHA_CORTE, FECHA_VACUNACION, count (case when DOSIS = 1 then 1 end) as DOSIS1, count (case when DOSIS = 2 then 1 end) as DOSIS2 from df group by FECHA_VACUNACION """
+result_query_dosis=ps.sqldf(query_dosis,locals())
+print(ps.sqldf(query_dosis,locals()))
 
 # Please write the full query on one line
-# ACUMULADO DOSIS 2
-query_dosis2="""select FECHA_CORTE, FECHA_VACUNACION, DOSIS, count (rowid) as TOTAL, sum(count (rowid)) over (order by FECHA_VACUNACION) as ACUMULADO from df where "DOSIS" = 2 group by FECHA_VACUNACION"""
-result_query_dosis2=ps.sqldf(query_dosis2,locals())
-print(ps.sqldf(query_dosis2,locals()))
+# ACUMULADOS DOSIS 1 Y 2
+query_acumulados="""select FECHA_CORTE, FECHA_VACUNACION, sum(count (case when DOSIS = 1 then 1 end)) over (order by FECHA_VACUNACION) as ACUMULADO1, sum(count (case when DOSIS = 2 then 1 end)) over (order by FECHA_VACUNACION) as ACUMULADO2 from df group by FECHA_VACUNACION"""
+result_query_acumulados=ps.sqldf(query_acumulados,locals())
+print(ps.sqldf(query_acumulados,locals()))
 
 # Please write the full query on one line
 # ACUMULADO DEPARTAMENTOS
@@ -57,6 +57,6 @@ result_query_departamentos['POBLACION']=col_poblacion
 result_query_departamentos['INDICE1']=round(result_query_departamentos['DOSIS1']/(result_query_departamentos['POBLACION']/100000)).astype('int')
 result_query_departamentos['INDICE2']=round(result_query_departamentos['DOSIS2']/(result_query_departamentos['POBLACION']/100000)).astype('int')
 
-result_query_dosis1.to_csv('resultados/dosis1.csv')
-result_query_dosis2.to_csv('resultados/dosis2.csv')
+result_query_dosis.to_csv('resultados/dosis1y2.csv')
+result_query_acumulados.to_csv('resultados/acumulados1y2.csv')
 result_query_departamentos.to_csv('resultados/departamentos.csv')
