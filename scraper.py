1478b81647a591dcf6302b0038ddcd8fe82a04de
Anna Absi <anna.absi@gmail.com>
2021-05-11 19:24:09 -0500

    indices por sexo

--- scraper.py
+++ scraper.py
@@ -26,37 +26,74 @@ query_departamentos="""select FECHA_CORTE, DEPARTAMENTO, count (case when DOSIS 
 result_query_departamentos=ps.sqldf(query_departamentos,locals())
 print(ps.sqldf(query_departamentos,locals()))
 
-col_poblacion=[426806,
-1180638,
-430736,
-1497438,
-668213,
-1453711,
-1129854,
-1357075,
-365317,
-760267,
-975182,
-1361467,
-2016771,
-1310785,
-10628470,
-1027559,
-173811,
-192740,
-271904,
-2047954,
-1237997,
-899648,
-370974,
-251521,
-589110]
+# Please write the full query on one line
+# ACUMULADO DEPARTAMENTOS POR SEXO
+query_departamentos_sexo="""select FECHA_CORTE, DEPARTAMENTO, count (case when SEXO = 'MASCULINO' and DOSIS = 2 then 1 end) as DOSIS2_M, count (case when SEXO = 'FEMENINO' and DOSIS = 2 then 1 end) as DOSIS2_F from df group by DEPARTAMENTO"""
+result_query_departamentos_sexo=ps.sqldf(query_departamentos_sexo,locals())
+print(ps.sqldf(query_departamentos_sexo,locals()))
 
-result_query_departamentos['POBLACION']=col_poblacion
+col_poblacion_m=[219801,
+594832,
+220370,
+735707,
+341951,
+727265,
+550046,
+686543,
+184121,
+384345,
+488836,
+678494,
+1000002,
+638228,
+5119560,
+531000,
+98215,
+102855,
+140252,
+1030975,
+611616,
+474458,
+188152,
+135675,
+307596]
 
-result_query_departamentos['INDICE1']=round(result_query_departamentos['DOSIS1']/(result_query_departamentos['POBLACION']/100000)).astype('int')
-result_query_departamentos['INDICE2']=round(result_query_departamentos['DOSIS2']/(result_query_departamentos['POBLACION']/100000)).astype('int')
+col_poblacion_f=[207005,
+585806,
+210366,
+761731,
+326262,
+726446,
+579808,
+670532,
+181196,
+375922,
+486346,
+682973,
+1016769,
+672557,
+5508910,
+496559,
+75596,
+89885,
+131652,
+1016979,
+626381,
+425190,
+182822,
+115846,
+281514]
+
+result_query_departamentos_sexo['POB_M']=col_poblacion_m
+result_query_departamentos_sexo['POB_F']=col_poblacion_f
+result_query_departamentos['POBLACION']=result_query_departamentos_sexo['POB_M'] + result_query_departamentos_sexo['POB_F']
+
+result_query_departamentos['INDICE']=round(result_query_departamentos['DOSIS2']/(result_query_departamentos['POBLACION']/100000)).astype('int')
+a = result_query_departamentos_sexo['POB_M']/result_query_departamentos_sexo['POB_F']
+result_query_departamentos_sexo['INDICE_M']=round(result_query_departamentos_sexo['DOSIS2_M']/(result_query_departamentos_sexo['POB_M']/(a*100000/(a+1)))).astype('int')
+result_query_departamentos_sexo['INDICE_F']=round(result_query_departamentos_sexo['DOSIS2_F']/(result_query_departamentos_sexo['POB_F']/(100000/(a+1)))).astype('int')
 
 result_query_dosis.to_csv('resultados/dosis1y2.csv')
 result_query_acumulados.to_csv('resultados/acumulados1y2.csv')
 result_query_departamentos.to_csv('resultados/departamentos.csv')
+result_query_departamentos_sexo.to_csv('resultados/departamentos_sexo.csv')
