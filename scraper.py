524f5afcbedc6749f6c6ce5c218b0baf2129f576
Anna Absi <anna.absi@gmail.com>
2021-05-22 23:02:17 -0500

    refactoring

--- scraper.py
+++ scraper.py
@@ -1,112 +1,74 @@
 import pandas as pd
-import pandasql as ps
 
-URL_DATA='https://cloud.minsa.gob.pe/s/ZgXoXqK2KLjRLxD/download'
-df=pd.read_csv(URL_DATA)
+df=pd.read_csv('https://cloud.minsa.gob.pe/s/ZgXoXqK2KLjRLxD/download', usecols=['FECHA_CORTE', 'EDAD', 'SEXO', 'FECHA_VACUNACION', 'DOSIS', 'DEPARTAMENTO'],parse_dates=['FECHA_CORTE', 'FECHA_VACUNACION'])
+#df=pd.read_csv('vacunas_covid.csv', usecols=['FECHA_CORTE', 'EDAD', 'SEXO', 'FECHA_VACUNACION', 'DOSIS', 'DEPARTAMENTO'], parse_dates=['FECHA_CORTE', 'FECHA_VACUNACION'])
+FECHA_CORTE=df['FECHA_CORTE'].unique()
+FECHA_CORTE=pd.to_datetime(FECHA_CORTE[0]).strftime('%Y-%m-%d')
 
+#DIARIO DOSIS 1 Y DOSIS 2
+df_ambas_dosis=df[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
+df_ambas_dosis=df_ambas_dosis.reset_index()
+df_ambas_dosis=df_ambas_dosis.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
+df_ambas_dosis=df_ambas_dosis.rename_axis(None, axis=1)
+df_ambas_dosis.columns=['DOSIS1','DOSIS2']
+df_ambas_dosis=df_ambas_dosis.fillna(0).astype('int')
+df_ambas_dosis
 
-# Delete all not wanted columns
-df.drop(['UUID', 'GRUPO_RIESGO', 'FABRICANTE', 'DIRESA', 'PROVINCIA', 'DISTRITO'], axis=1, inplace=True)
+#ACUMULADO DOSIS 1 Y DOSIS 2
+df_ambas_dosis_cum=df_ambas_dosis.cumsum()
+df_ambas_dosis_cum
 
-# Please write the full query on one line
-# DOSIS 1 y 2
-query_dosis="""select FECHA_CORTE, FECHA_VACUNACION, count (case when DOSIS = 1 then 1 end) as DOSIS1, count (case when DOSIS = 2 then 1 end) as DOSIS2 from df group by FECHA_VACUNACION """
-result_query_dosis=ps.sqldf(query_dosis,locals())
-print(ps.sqldf(query_dosis,locals()))
+col_poblacion=[426806,
+1180638,
+430736,
+1497438,
+668213,
+1453711,
+1129854,
+1357075,
+365317,
+760267,
+975182,
+1361467,
+2016771,
+1310785,
+10628470,
+1027559,
+173811,
+192740,
+271904,
+2047954,
+1237997,
+899648,
+370974,
+251521,
+589110]
 
-# Please write the full query on one line
-# ACUMULADOS DOSIS 1 Y 2
-query_acumulados="""select FECHA_CORTE, FECHA_VACUNACION, sum(count (case when DOSIS = 1 then 1 end)) over (order by FECHA_VACUNACION) as ACUMULADO1, sum(count (case when DOSIS = 2 then 1 end)) over (order by FECHA_VACUNACION) as ACUMULADO2 from df group by FECHA_VACUNACION"""
-result_query_acumulados=ps.sqldf(query_acumulados,locals())
-print(ps.sqldf(query_acumulados,locals()))
+# ACUMULADO POR DEPARTAMENTO (DOSIS 2 - VACUNACION COMPLETA)
+df_ambas_dosis_departamento=df[['DEPARTAMENTO', 'SEXO']].groupby(['DEPARTAMENTO']).count()
+df_ambas_dosis_departamento=df_ambas_dosis_departamento.reset_index()
+df_ambas_dosis_departamento.rename(columns = {'SEXO':'DOSIS2'}, inplace = True)
+df_ambas_dosis_departamento_dosis2=df_ambas_dosis_departamento[['DEPARTAMENTO','DOSIS2']]
+df_ambas_dosis_departamento_dosis2=df_ambas_dosis_departamento_dosis2.reset_index(drop=True)
+df_ambas_dosis_departamento_dosis2['POBLACION']=col_poblacion
+df_ambas_dosis_departamento_dosis2['INDICE']=round(df_ambas_dosis_departamento_dosis2['DOSIS2']/(df_ambas_dosis_departamento_dosis2['POBLACION']/100000)).astype('int')
+df_ambas_dosis_departamento_dosis2=df_ambas_dosis_departamento_dosis2.set_index('DEPARTAMENTO')
+df_ambas_dosis_departamento_dosis2
 
-# Please write the full query on one line
-# ACUMULADO DEPARTAMENTOS
-query_departamentos="""select FECHA_CORTE, DEPARTAMENTO, count (case when DOSIS = 1 then 1 end) as DOSIS1, count (case when DOSIS = 2 then 1 end) as DOSIS2 from df group by DEPARTAMENTO"""
-result_query_departamentos=ps.sqldf(query_departamentos,locals())
-print(ps.sqldf(query_departamentos,locals()))
-
-# Please write the full query on one line
-# ACUMULADO DEPARTAMENTOS POR SEXO
-query_departamentos_sexo="""select FECHA_CORTE, DEPARTAMENTO, count (case when SEXO = 'MASCULINO' and DOSIS = 2 then 1 end) as DOSIS2_M, count (case when SEXO = 'FEMENINO' and DOSIS = 2 then 1 end) as DOSIS2_F from df group by DEPARTAMENTO"""
-result_query_departamentos_sexo=ps.sqldf(query_departamentos_sexo,locals())
-print(ps.sqldf(query_departamentos_sexo,locals()))
-
-# ACUMULADO POR GRUPO ETARIO
+# ACUMULADO POR GRUPO ETARIO (DOSIS 2 - VACUNACION COMPLETA)
 bins = [18,20,30,40,50,60,70,80,df['EDAD'].max()+1]
 labels = ['18 a 19 años','20 a 29 años','30 a 39 años','40 a 49 años','50 a 59 años','60 a 69 años','70 a 79 años','80 años a más']
 poblacion_por_grupo_etario = [900000,5700000,5300000,4400000,3300000,2300000,1300000,700000] 
-result_df_edades = df
-result_df_edades.drop(['SEXO', 'FECHA_VACUNACION', 'DEPARTAMENTO'], axis=1, inplace=True)
-result_df_edades['GRUPO_ETARIO'] = pd.cut(df['EDAD'], bins=bins, labels=labels, right=False)
-result_query_edades = result_df_edades[result_df_edades.DOSIS == 2].groupby(['GRUPO_ETARIO'])["DOSIS"].count().reset_index()
-result_query_edades['POBLACION']=poblacion_por_grupo_etario
-result_query_edades['%']=round(result_query_edades['DOSIS']/result_query_edades['POBLACION']*100,2)
-result_query_edades
+df_edades = df
+df_edades['GRUPO_ETARIO'] = pd.cut(df['EDAD'], bins=bins, labels=labels, right=False)
+df_edades = df_edades[df_edades.DOSIS == 2].groupby(['GRUPO_ETARIO'])["DOSIS"].count().reset_index()
+df_edades.rename(columns = {'DOSIS':'DOSIS2'}, inplace = True)
+df_edades['POBLACION']=poblacion_por_grupo_etario
+df_edades['PORCENTAJE']=round(df_edades['DOSIS2']/df_edades['POBLACION']*100,2)
+df_edades=df_edades.set_index('GRUPO_ETARIO')
+df_edades
 
-col_poblacion_m=[219801,
-594832,
-220370,
-735707,
-341951,
-727265,
-550046,
-686543,
-184121,
-384345,
-488836,
-678494,
-1000002,
-638228,
-5119560,
-531000,
-98215,
-102855,
-140252,
-1030975,
-611616,
-474458,
-188152,
-135675,
-307596]
-
-col_poblacion_f=[207005,
-585806,
-210366,
-761731,
-326262,
-726446,
-579808,
-670532,
-181196,
-375922,
-486346,
-682973,
-1016769,
-672557,
-5508910,
-496559,
-75596,
-89885,
-131652,
-1016979,
-626381,
-425190,
-182822,
-115846,
-281514]
-
-result_query_departamentos_sexo['POB_M']=col_poblacion_m
-result_query_departamentos_sexo['POB_F']=col_poblacion_f
-result_query_departamentos['POBLACION']=result_query_departamentos_sexo['POB_M'] + result_query_departamentos_sexo['POB_F']
-
-result_query_departamentos['INDICE']=round(result_query_departamentos['DOSIS2']/(result_query_departamentos['POBLACION']/100000)).astype('int')
-a = result_query_departamentos_sexo['POB_M']/result_query_departamentos_sexo['POB_F']
-result_query_departamentos_sexo['INDICE_M']=round(result_query_departamentos_sexo['DOSIS2_M']/(result_query_departamentos_sexo['POB_M']/(a*100000/(a+1)))).astype('int')
-result_query_departamentos_sexo['INDICE_F']=round(result_query_departamentos_sexo['DOSIS2_F']/(result_query_departamentos_sexo['POB_F']/(100000/(a+1)))).astype('int')
-
-result_query_dosis.to_csv('resultados/dosis1y2.csv')
-result_query_acumulados.to_csv('resultados/acumulados1y2.csv')
-result_query_departamentos.to_csv('resultados/departamentos.csv')
-result_query_departamentos_sexo.to_csv('resultados/departamentos_sexo.csv')
-result_query_edades.to_csv('resultados/dosis2_por_edades.csv')
+df_ambas_dosis.to_csv('resultados/dosis1y2.csv')
+df_ambas_dosis_cum.to_csv('resultados/acumulados1y2.csv')
+df_ambas_dosis_departamento_dosis2.to_csv('resultados/departamentos.csv')
+df_edades.to_csv('resultados/dosis2_por_edades.csv')
