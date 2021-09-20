8b6b83c9e61bd9f0e09452c223db3c81f779b33c
Anna Absi <anna.absi@gmail.com>
2021-09-20 18:47:33 -0500

    optimizando github actions leyendo hashes

--- scraper-sinadef.py
+++ scraper-sinadef.py
@@ -1,7 +1,9 @@
-import numpy as np
-import pandas as pd
 import subprocess
 from datetime import date, timedelta
+
+import numpy as np
+import pandas as pd
+from py_essentials import hashing as hs
 
 def _execute_shell_command(command):
     with subprocess.Popen(command,
@@ -13,58 +15,79 @@ def _execute_shell_command(command):
         return stdout, stderr
 
 try:
-    _, _ = _execute_shell_command(['wget', 'https://cloud.minsa.gob.pe/s/NctBnHXDnocgWAg/download', '-O', 'minsa.zip'])
-    _, _ = _execute_shell_command(['unzip', 'minsa.zip'])
+    _, _ = _execute_shell_command(['wget', 'https://cloud.minsa.gob.pe/s/NctBnHXDnocgWAg/download'])
 
-    xls = pd.read_excel('./DATA_SINADEF/SINADEF_DATOS_ABIERTOS.xlsx',engine='openpyxl', index_col=0)  # <-- add .read()
-    # xls
 
-    xls.drop(['DEBIDO A (CAUSA A)', 'CAUSA A (CIE-X)', 'DEBIDO A (CAUSA B)', 'CAUSA B (CIE-X)', 'DEBIDO A (CAUSA C)', 'CAUSA C (CIE-X)','DEBIDO A (CAUSA D)', 'CAUSA D (CIE-X)', 'DEBIDO A (CAUSA E)', 'CAUSA E (CIE-X)', 'DEBIDO A (CAUSA F)', 'CAUSA F (CIE-X)'], axis=1, inplace=True)
+    hash_downloaded = hs.fileChecksum("download", "sha256")    
 
-    xls = xls.rename(columns={"COD# UBIGEO DOMICILIO": "UBIGEO", "PAIS DOMICILIO": "PAIS","DEPARTAMENTO DOMICILIO": "DEPARTAMENTO", "PROVINCIA DOMICILIO": "PROVINCIA", "DISTRITO DOMICILIO": "DISTRITO"})
+    # Get hash saved
+    with open('resultados/hashes/hash_sinadef.txt') as f:
+        hash_saved = f.readlines()[0]
 
-    xls = xls.sort_values(by='FECHA')
-    xls['FECHA'] = pd.to_datetime(xls['FECHA'], errors='coerce', dayfirst=True)
-    xls['FECHA'] = pd.to_datetime(xls['FECHA'], format='%y-%m-%d', errors='ignore')
-    # xls
+    print(hash_downloaded)
+    print(hash_saved)
 
-    xls['FECHA'].replace('SIN REGISTRO', np.nan)
-    xls['EDAD'].replace('SIN REGISTRO', np.nan)
-    xls['FECHA'].dropna()
-    xls['EDAD'].dropna()
+    if hash_saved==hash_downloaded:
+        print("SAME FILE")
+    else:
+        print("DIFFERENT FILE")
 
-    start_date = "2019-01-01"
-    yesterday = date.today() - timedelta(days=1)
-    end_date = yesterday.strftime("%Y-%m-%d")
-    after_start_date = (xls["FECHA"] >= start_date) & (xls["FECHA"] < end_date)
-    filtered_dates = xls.loc[after_start_date]
+        _, _ = _execute_shell_command(['unzip', 'download'])
 
-    xls_edad_anos = filtered_dates[filtered_dates['TIEMPO EDAD'] == 'AÑOS']
-    # xls_edad_anos
+        xls = pd.read_excel('./DATA_SINADEF/SINADEF_DATOS_ABIERTOS.xlsx',engine='openpyxl', index_col=0)  # <-- add .read()
+        # xls
 
-    xls_edad_anos.EDAD = pd.to_numeric(xls_edad_anos.EDAD, errors='coerce')
-    # xls_edad_anos
+        xls.drop(['DEBIDO A (CAUSA A)', 'CAUSA A (CIE-X)', 'DEBIDO A (CAUSA B)', 'CAUSA B (CIE-X)', 'DEBIDO A (CAUSA C)', 'CAUSA C (CIE-X)','DEBIDO A (CAUSA D)', 'CAUSA D (CIE-X)', 'DEBIDO A (CAUSA E)', 'CAUSA E (CIE-X)', 'DEBIDO A (CAUSA F)', 'CAUSA F (CIE-X)'], axis=1, inplace=True)
 
-    df_total = xls_edad_anos.groupby(['FECHA', 'AÑO']).size().unstack()
-    df_total = df_total.rename(columns=str).reset_index()
-    # df_total
+        xls = xls.rename(columns={"COD# UBIGEO DOMICILIO": "UBIGEO", "PAIS DOMICILIO": "PAIS","DEPARTAMENTO DOMICILIO": "DEPARTAMENTO", "PROVINCIA DOMICILIO": "PROVINCIA", "DISTRITO DOMICILIO": "DISTRITO"})
 
-    df_total.to_csv('resultados/fallecidos_total.csv', index=False)
+        xls = xls.sort_values(by='FECHA')
+        xls['FECHA'] = pd.to_datetime(xls['FECHA'], errors='coerce', dayfirst=True)
+        xls['FECHA'] = pd.to_datetime(xls['FECHA'], format='%y-%m-%d', errors='ignore')
+        # xls
 
-    bins = [17, 59, 69, 79, 130]
+        xls['FECHA'].replace('SIN REGISTRO', np.nan)
+        xls['EDAD'].replace('SIN REGISTRO', np.nan)
+        xls['FECHA'].dropna()
+        xls['EDAD'].dropna()
 
-    rs = pd.cut(xls_edad_anos['EDAD'], bins)
+        start_date = "2019-01-01"
+        yesterday = date.today() - timedelta(days=1)
+        end_date = yesterday.strftime("%Y-%m-%d")
+        after_start_date = (xls["FECHA"] >= start_date) & (xls["FECHA"] < end_date)
+        filtered_dates = xls.loc[after_start_date]
 
-    df = xls_edad_anos.groupby(['FECHA', rs]).size().unstack()
-    df = df.rename(columns=str).reset_index()
-    df = df.rename(columns={'(17, 59]': '18-59', '(59, 69]': '60-69',
-                '(69, 79]': '70-79', '(79, 130]': '80+'})
-    # df
+        xls_edad_anos = filtered_dates[filtered_dates['TIEMPO EDAD'] == 'AÑOS']
+        # xls_edad_anos
 
-    df.to_csv('resultados/fallecidos_edad.csv', index=False)
+        xls_edad_anos.EDAD = pd.to_numeric(xls_edad_anos.EDAD, errors='coerce')
+        # xls_edad_anos
 
-    _, _ = _execute_shell_command(['rm','-rf', './DATA_SINADEF/'])
-    _, _ = _execute_shell_command(['rm','-f', 'minsa.zip'])
-    
+        df_total = xls_edad_anos.groupby(['FECHA', 'AÑO']).size().unstack()
+        df_total = df_total.rename(columns=str).reset_index()
+        # df_total
+
+        df_total.to_csv('resultados/fallecidos_total.csv', index=False)
+
+        bins = [17, 59, 69, 79, 130]
+
+        rs = pd.cut(xls_edad_anos['EDAD'], bins)
+
+        df = xls_edad_anos.groupby(['FECHA', rs]).size().unstack()
+        df = df.rename(columns=str).reset_index()
+        df = df.rename(columns={'(17, 59]': '18-59', '(59, 69]': '60-69',
+                    '(69, 79]': '70-79', '(79, 130]': '80+'})
+        # df
+
+        df.to_csv('resultados/fallecidos_edad.csv', index=False)
+
+        _, _ = _execute_shell_command(['rm','-rf', './DATA_SINADEF/'])
+        _, _ = _execute_shell_command(['rm','-f', 'download'])
+
+        # Save new hash
+        file = open('resultados/hashes/hash_sinadef.txt', 'w')
+        file.write(hash_downloaded)
+        file.close()
+
 except ConnectionResetError:
     pass
--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import requests
 import py7zr
+import requests
+from py_essentials import hashing as hs
 
 try:
     url = "https://cloud.minsa.gob.pe/s/To2QtqoNjKqobfw/download"
@@ -9,117 +10,133 @@ try:
 
     with open('vacunas_covid.7z', 'wb') as f:
         f.write(req.content)
-    
-    with py7zr.SevenZipFile('vacunas_covid.7z', mode='r') as z:
-        z.extractall()
 
-    df=pd.read_csv('vacunas_covid.csv', usecols=['FECHA_CORTE', 'EDAD', 'SEXO', 'FECHA_VACUNACION', 'DOSIS', 'DEPARTAMENTO', 'FABRICANTE'],parse_dates=['FECHA_VACUNACION'])
-    print(df.head(10))
-    #df=pd.read_csv('vacunas_covid.csv', usecols=['FECHA_CORTE', 'EDAD', 'SEXO', 'FECHA_VACUNACION', 'DOSIS', 'DEPARTAMENTO', 'FABRICANTE'], parse_dates=['FECHA_VACUNACION'])
-    fecha_corte=df['FECHA_CORTE'].drop_duplicates().set_axis(['fecha_corte'])
-    fecha_corte.to_json("resultados/fecha_corte.json")
+    hash_downloaded = hs.fileChecksum("vacunas_covid.7z", "sha256")
 
-    #DIARIO DOSIS 1 Y DOSIS 2
-    df_ambas_dosis=df[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
-    df_ambas_dosis=df_ambas_dosis.reset_index()
-    df_ambas_dosis=df_ambas_dosis.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
-    df_ambas_dosis=df_ambas_dosis.rename_axis(None, axis=1)
-    df_ambas_dosis.columns=['DOSIS1','DOSIS2']
-    df_ambas_dosis=df_ambas_dosis.fillna(0).astype('int')
-    df_ambas_dosis
+    # Get hash saved
+    with open('resultados/hashes/hash_scraper.txt') as f:
+        hash_saved = f.readlines()[0]
 
-    #ACUMULADO DOSIS 1 Y DOSIS 2
-    df_ambas_dosis_cum=df_ambas_dosis.cumsum()
-    df_ambas_dosis_cum
+    if hash_saved==hash_downloaded:
+        print("SAME FILE")
+    else:
+        print("DIFFERENT FILE")
 
-    col_poblacion=[426806,
-    1180638,
-    430736,
-    1497438,
-    668213,
-    1453711,
-    1129854,
-    1357075,
-    365317,
-    760267,
-    975182,
-    1361467,
-    2016771,
-    1310785,
-    10628470,
-    1027559,
-    173811,
-    192740,
-    271904,
-    2047954,
-    1237997,
-    899648,
-    370974,
-    251521,
-    589110]
+        with py7zr.SevenZipFile('vacunas_covid.7z', mode='r') as z:
+            z.extractall()
 
-    # ACUMULADO POR DEPARTAMENTO (DOSIS 2 - VACUNACION COMPLETA)
-    df_ambas_dosis_departamento=df[['DEPARTAMENTO','DOSIS','SEXO']].groupby(['DEPARTAMENTO', 'DOSIS']).count()
-    df_ambas_dosis_departamento=df_ambas_dosis_departamento.reset_index()
-    df_ambas_dosis_departamento=df_ambas_dosis_departamento.pivot(index='DEPARTAMENTO', columns='DOSIS', values='SEXO')
-    df_ambas_dosis_departamento.columns=['DOSIS1','DOSIS2']
-    df_ambas_dosis_departamento['POBLACION']=col_poblacion
-    df_ambas_dosis_departamento['INDICE']=round(df_ambas_dosis_departamento['DOSIS2']/(df_ambas_dosis_departamento['POBLACION']/100000)).astype('int')
-    df_ambas_dosis_departamento
+        df=pd.read_csv('vacunas_covid.csv', usecols=['FECHA_CORTE', 'EDAD', 'SEXO', 'FECHA_VACUNACION', 'DOSIS', 'DEPARTAMENTO', 'FABRICANTE'],parse_dates=['FECHA_VACUNACION'])
+        print(df.head(10))
+        #df=pd.read_csv('vacunas_covid.csv', usecols=['FECHA_CORTE', 'EDAD', 'SEXO', 'FECHA_VACUNACION', 'DOSIS', 'DEPARTAMENTO', 'FABRICANTE'], parse_dates=['FECHA_VACUNACION'])
+        fecha_corte=df['FECHA_CORTE'].drop_duplicates().set_axis(['fecha_corte'])
+        fecha_corte.to_json("resultados/fecha_corte.json")
 
-    # ACUMULADO POR GRUPO ETARIO (DOSIS 2 - VACUNACION COMPLETA)
-    bins = [18,30,40,50,60,70,80,df['EDAD'].max()+1]
-    labels = ['18 a 29 años','30 a 39 años','40 a 49 años','50 a 59 años','60 a 69 años','70 a 79 años','80 años a más']
-    poblacion_por_grupo_etario = [6303670,5031117,4183174,3277134,2221241,1271842,647355] 
-    df_edades = df
-    df_edades['GRUPO_ETARIO'] = pd.cut(df['EDAD'], bins=bins, labels=labels, right=False)
-    df_edades = df_edades[df_edades.DOSIS == 2].groupby(['GRUPO_ETARIO'])["DOSIS"].count().reset_index()
-    df_edades.rename(columns = {'DOSIS':'DOSIS2'}, inplace = True)
-    df_edades['POBLACION']=poblacion_por_grupo_etario
-    df_edades['PORCENTAJE']=round(df_edades['DOSIS2']/df_edades['POBLACION']*100,2)
-    df_edades=df_edades.set_index('GRUPO_ETARIO')
-    df_edades
+        #DIARIO DOSIS 1 Y DOSIS 2
+        df_ambas_dosis=df[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
+        df_ambas_dosis=df_ambas_dosis.reset_index()
+        df_ambas_dosis=df_ambas_dosis.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
+        df_ambas_dosis=df_ambas_dosis.rename_axis(None, axis=1)
+        df_ambas_dosis.columns=['DOSIS1','DOSIS2']
+        df_ambas_dosis=df_ambas_dosis.fillna(0).astype('int')
+        df_ambas_dosis
 
-    #DIARIO POR FABRICANTE
-    df_fabricante=df[['FECHA_VACUNACION','FABRICANTE','SEXO']].groupby(['FECHA_VACUNACION','FABRICANTE']).count()
-    df_fabricante=df_fabricante.reset_index()
-    df_fabricante=df_fabricante.pivot(index='FECHA_VACUNACION', columns='FABRICANTE', values='SEXO')
-    df_fabricante=df_fabricante.rename_axis(None, axis=1)
-    df_fabricante=df_fabricante.fillna(0).astype('int')
-    df_fabricante
+        #ACUMULADO DOSIS 1 Y DOSIS 2
+        df_ambas_dosis_cum=df_ambas_dosis.cumsum()
+        df_ambas_dosis_cum
 
-    #ACUMULADO POR FABRICANTE
-    df_fabricante_cum=df_fabricante.cumsum()
-    df_fabricante_cum
+        col_poblacion=[426806,
+        1180638,
+        430736,
+        1497438,
+        668213,
+        1453711,
+        1129854,
+        1357075,
+        365317,
+        760267,
+        975182,
+        1361467,
+        2016771,
+        1310785,
+        10628470,
+        1027559,
+        173811,
+        192740,
+        271904,
+        2047954,
+        1237997,
+        899648,
+        370974,
+        251521,
+        589110]
 
-    #DATA PONGO EL HOMBRO
-    from requests_html import HTMLSession
-    session = HTMLSession()
-    r = session.get('https://www.gob.pe/pongoelhombro#contador-de-vacunados')
-    scrap_contador_vacunados = r.html.find('.font-bold.text-3xl')
-    d = []
-    for number in scrap_contador_vacunados:
-        d.append(number.text)
-    contador_vacunados = pd.Series(d,index=['total_dosis1', 'total_dosis2','ayer_dosis1','ayer_dosis2','total_dosis','ayer_total_dosis','vacunacion_completa'])
-    contador_vacunados.to_json('resultados/pongo_el_hombro.json')
+        # ACUMULADO POR DEPARTAMENTO (DOSIS 2 - VACUNACION COMPLETA)
+        df_ambas_dosis_departamento=df[['DEPARTAMENTO','DOSIS','SEXO']].groupby(['DEPARTAMENTO', 'DOSIS']).count()
+        df_ambas_dosis_departamento=df_ambas_dosis_departamento.reset_index()
+        df_ambas_dosis_departamento=df_ambas_dosis_departamento.pivot(index='DEPARTAMENTO', columns='DOSIS', values='SEXO')
+        df_ambas_dosis_departamento.columns=['DOSIS1','DOSIS2']
+        df_ambas_dosis_departamento['POBLACION']=col_poblacion
+        df_ambas_dosis_departamento['INDICE']=round(df_ambas_dosis_departamento['DOSIS2']/(df_ambas_dosis_departamento['POBLACION']/100000)).astype('int')
+        df_ambas_dosis_departamento
 
-    #DIARIO DOSIS 1 Y DOSIS 2: TACNA
-    df_tacna=df[df['DEPARTAMENTO'] == 'TACNA']
-    df_diario_tacna=df_tacna[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
-    df_diario_tacna=df_diario_tacna.reset_index()
-    df_diario_tacna=df_diario_tacna.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
-    df_diario_tacna=df_diario_tacna.rename_axis(None, axis=1)
-    df_diario_tacna.columns=['DOSIS1','DOSIS2']
-    df_diario_tacna=df_diario_tacna.fillna(0).astype('int')
-    df_diario_tacna
+        # ACUMULADO POR GRUPO ETARIO (DOSIS 2 - VACUNACION COMPLETA)
+        bins = [18,30,40,50,60,70,80,df['EDAD'].max()+1]
+        labels = ['18 a 29 años','30 a 39 años','40 a 49 años','50 a 59 años','60 a 69 años','70 a 79 años','80 años a más']
+        poblacion_por_grupo_etario = [6303670,5031117,4183174,3277134,2221241,1271842,647355] 
+        df_edades = df
+        df_edades['GRUPO_ETARIO'] = pd.cut(df['EDAD'], bins=bins, labels=labels, right=False)
+        df_edades = df_edades[df_edades.DOSIS == 2].groupby(['GRUPO_ETARIO'])["DOSIS"].count().reset_index()
+        df_edades.rename(columns = {'DOSIS':'DOSIS2'}, inplace = True)
+        df_edades['POBLACION']=poblacion_por_grupo_etario
+        df_edades['PORCENTAJE']=round(df_edades['DOSIS2']/df_edades['POBLACION']*100,2)
+        df_edades=df_edades.set_index('GRUPO_ETARIO')
+        df_edades
 
-    df_ambas_dosis.to_csv('resultados/dosis1y2.csv')
-    df_ambas_dosis_cum.to_csv('resultados/acumulados1y2.csv')
-    df_ambas_dosis_departamento.to_csv('resultados/departamentos.csv')
-    df_edades.to_csv('resultados/dosis2_por_edades.csv')
-    df_fabricante.to_csv('resultados/diario_por_fabricante.csv')
-    df_fabricante_cum.to_csv('resultados/acumulado_por_fabricante.csv')
-    df_diario_tacna.to_csv('resultados/tacna.csv')
+        #DIARIO POR FABRICANTE
+        df_fabricante=df[['FECHA_VACUNACION','FABRICANTE','SEXO']].groupby(['FECHA_VACUNACION','FABRICANTE']).count()
+        df_fabricante=df_fabricante.reset_index()
+        df_fabricante=df_fabricante.pivot(index='FECHA_VACUNACION', columns='FABRICANTE', values='SEXO')
+        df_fabricante=df_fabricante.rename_axis(None, axis=1)
+        df_fabricante=df_fabricante.fillna(0).astype('int')
+        df_fabricante
+
+        #ACUMULADO POR FABRICANTE
+        df_fabricante_cum=df_fabricante.cumsum()
+        df_fabricante_cum
+
+        #DATA PONGO EL HOMBRO
+        from requests_html import HTMLSession
+        session = HTMLSession()
+        r = session.get('https://www.gob.pe/pongoelhombro#contador-de-vacunados')
+        scrap_contador_vacunados = r.html.find('.font-bold.text-3xl')
+        d = []
+        for number in scrap_contador_vacunados:
+            d.append(number.text)
+        contador_vacunados = pd.Series(d,index=['total_dosis1', 'total_dosis2','ayer_dosis1','ayer_dosis2','total_dosis','ayer_total_dosis','vacunacion_completa'])
+        contador_vacunados.to_json('resultados/pongo_el_hombro.json')
+
+        #DIARIO DOSIS 1 Y DOSIS 2: TACNA
+        df_tacna=df[df['DEPARTAMENTO'] == 'TACNA']
+        df_diario_tacna=df_tacna[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
+        df_diario_tacna=df_diario_tacna.reset_index()
+        df_diario_tacna=df_diario_tacna.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
+        df_diario_tacna=df_diario_tacna.rename_axis(None, axis=1)
+        df_diario_tacna.columns=['DOSIS1','DOSIS2']
+        df_diario_tacna=df_diario_tacna.fillna(0).astype('int')
+        df_diario_tacna
+
+        df_ambas_dosis.to_csv('resultados/dosis1y2.csv')
+        df_ambas_dosis_cum.to_csv('resultados/acumulados1y2.csv')
+        df_ambas_dosis_departamento.to_csv('resultados/departamentos.csv')
+        df_edades.to_csv('resultados/dosis2_por_edades.csv')
+        df_fabricante.to_csv('resultados/diario_por_fabricante.csv')
+        df_fabricante_cum.to_csv('resultados/acumulado_por_fabricante.csv')
+        df_diario_tacna.to_csv('resultados/tacna.csv')
+
+        # Save new hash
+        file = open('resultados/hashes/hash_scraper.txt', 'w')
+        file.write(hash_downloaded)
+        file.close()
 
 except ConnectionResetError:
     # error de peers
