b7c3ed5b792ad6e72ca4d15d3f5aa564316b1a44
Anna Absi <anna.absi@gmail.com>
2021-10-26 23:20:00 -0500

    url camas uci

--- scraper-uci.py
+++ scraper-uci.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import requests
-from io import StringIO
 
 try:
-    url = "http://datos.susalud.gob.pe/node/548/download"
+    url = "http://datos.susalud.gob.pe/sites/default/files/Camas_26-10-2021.csv"
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
     req = requests.get(url, headers=headers)
-    data = StringIO(req.text)
+
+    with open('camas_uci.csv', 'wb') as f:
+        f.write(req.content)
     
-    df=pd.read_csv(data, usecols=['FECHACORTE', 'REGION', 'ZC_UCI_ADUL_CAM_TOTAL','ZC_UCI_ADUL_CAM_INOPERATIVOS', 'ZC_UCI_ADUL_CAM_TOT_OPER', 'ZC_UCI_ADUL_CAM_TOT_DISP', 'ZC_UCI_ADUL_CAM_TOT_OCUP'], sep='|')
+    df=pd.read_csv('camas_uci.csv', usecols=['FECHACORTE', 'REGION', 'ZC_UCI_ADUL_CAM_TOTAL','ZC_UCI_ADUL_CAM_INOPERATIVOS', 'ZC_UCI_ADUL_CAM_TOT_OPER', 'ZC_UCI_ADUL_CAM_TOT_DISP', 'ZC_UCI_ADUL_CAM_TOT_OCUP'], sep='|')
     fecha_corte = df['FECHACORTE'].tail(1)
 
     df_camas_uci=df[df['FECHACORTE'] == fecha_corte.values[0]].reset_index(drop=True)
