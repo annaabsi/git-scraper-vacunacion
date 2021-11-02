64dc85fd4f7b3be46d3e890bb887093f386644da
Anna Absi <anna.absi@gmail.com>
2021-11-01 23:33:07 -0500

    scraper-uci nuevo enlace

--- scraper-uci.py
+++ scraper-uci.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 
 try:
-    url = "http://datos.susalud.gob.pe/sites/default/files/Camas_26-10-2021.csv"
+    url = "http://datos.susalud.gob.pe/node/548/download"
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
     req = requests.get(url, headers=headers)
 
