0de65931fef9b0bc3de10fa2581543a04705fd5d
Anna Absi <anna.absi@gmail.com>
2021-11-02 00:12:58 -0500

    python3 version

--- scraper-uci.py
+++ scraper-uci.py
@@ -3,8 +3,7 @@ import requests
 
 try:
     url = "http://datos.susalud.gob.pe/node/548/download"
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
-    req = requests.get(url, headers=headers)
+    req = requests.get(url)
 
     with open('camas_uci.csv', 'wb') as f:
         f.write(req.content)
