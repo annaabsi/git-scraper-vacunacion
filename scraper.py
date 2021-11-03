606b7688ae3189fe24e8739ca570bbb380f959da
Anna Absi <anna.absi@gmail.com>
2021-11-02 20:25:50 -0500

    by department

--- scraper.py
+++ scraper.py
@@ -3,6 +3,18 @@ import py7zr
 import requests
 from py_essentials import hashing as hs
 
+
+def summary_by_department(df):
+
+    df=df[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
+    df=df.reset_index()
+    df=df.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
+    df=df.rename_axis(None, axis=1)
+    df.columns=['DOSIS1','DOSIS2','DOSIS3']
+    df=df.fillna(0).astype('int')
+    return df
+
+
 try:
     url = "https://cloud.minsa.gob.pe/s/To2QtqoNjKqobfw/download"
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
@@ -116,35 +128,37 @@ try:
         contador_vacunados = pd.Series(d,index=['total_dosis1', 'total_dosis2','ayer_dosis1','ayer_dosis2','total_dosis','ayer_total_dosis','vacunacion_completa'])
         contador_vacunados.to_json('resultados/pongo_el_hombro.json')
 
-        # DIARIO TACNA
-        df_tacna=df[df['DEPARTAMENTO'] == 'TACNA']
-        df_diario_tacna=df_tacna[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
-        df_diario_tacna=df_diario_tacna.reset_index()
-        df_diario_tacna=df_diario_tacna.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
-        df_diario_tacna=df_diario_tacna.rename_axis(None, axis=1)
-        df_diario_tacna.columns=['DOSIS1','DOSIS2','DOSIS3']
-        df_diario_tacna=df_diario_tacna.fillna(0).astype('int')
-        df_diario_tacna
+        # DIARIO POR DEPARTAMENTO
+        list_departamentos = ["AMAZONAS",
+                         "ANCASH",
+                         "APURIMAC",
+                         "AREQUIPA",
+                         "AYACUCHO",
+                         "CAJAMARCA",
+                         "CALLAO",
+                         "CUSCO",
+                         "HUANCAVELICA",
+                         "HUANUCO",
+                         "ICA",
+                         "JUNIN",
+                         "LA LIBERTAD",
+                         "LAMBAYEQUE",
+                         "LIMA",
+                         "LORETO",
+                         "MADRE DE DIOS",
+                         "MOQUEGUA",
+                         "PASCO",
+                         "PIURA",
+                         "PUNO",
+                         "SAN MARTIN",
+                         "TACNA",
+                         "TUMBES",
+                         "UCAYALI"]
 
-        #DIARIO PUNO
-        df_puno=df[df['DEPARTAMENTO'] == 'PUNO']
-        df_diario_puno=df_tacna[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
-        df_diario_puno=df_diario_puno.reset_index()
-        df_diario_puno=df_diario_puno.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
-        df_diario_puno=df_diario_puno.rename_axis(None, axis=1)
-        df_diario_puno.columns=['DOSIS1','DOSIS2','DOSIS3']
-        df_diario_puno=df_diario_puno.fillna(0).astype('int')
-        df_diario_puno
-
-        # DIARIO MADRE DE DIOS
-        df_mdd=df[df['DEPARTAMENTO'] == 'MADRE DE DIOS']
-        df_diario_mdd=df_mdd[['FECHA_VACUNACION','DOSIS','SEXO']].groupby(['FECHA_VACUNACION','DOSIS']).count()
-        df_diario_mdd=df_diario_mdd.reset_index()
-        df_diario_mdd=df_diario_mdd.pivot(index='FECHA_VACUNACION', columns='DOSIS', values='SEXO')
-        df_diario_mdd=df_diario_mdd.rename_axis(None, axis=1)
-        df_diario_mdd.columns=['DOSIS1','DOSIS2','DOSIS3']
-        df_diario_mdd=df_diario_mdd.fillna(0).astype('int')
-        df_diario_mdd
+        for department_name in list_departamentos:
+            df_by_department=df[df['DEPARTAMENTO'] == department_name]
+            df_filtered=summary_by_department(df_by_department)
+            df_filtered.to_csv(f"resultados/departamentos/{department_name.lower()}.csv")
 
         # TOTAL DE 11 A 16 AÑOS
         bins = [11,12,13,14,15,16,17]
@@ -171,9 +185,9 @@ try:
         df_edades.to_csv('resultados/dosis2_por_edades.csv')
         df_fabricante.to_csv('resultados/diario_por_fabricante.csv')
         df_fabricante_cum.to_csv('resultados/acumulado_por_fabricante.csv')
-        df_diario_tacna.to_csv('resultados/tacna.csv')
-        df_diario_puno.to_csv('resultados/puno.csv')
-        df_diario_mdd.to_csv('resultados/madre_de_dios.csv')
+
+
+
         df_11_16.to_csv('resultados/11_a_16_anios.csv')
         df_11_16_diario_cum.to_csv('resultados/acumulado_11_a_16_anios.csv')
 
